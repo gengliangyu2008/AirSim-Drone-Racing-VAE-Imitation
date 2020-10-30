895ac269a7c64bd6cc4c7f324b2e4f20c988211b
Gary <gengliangyu2008@gmail.com>
2020-10-30 22:45:26 +0800

    remove unused codes in DensetNet121

--- racing_models/dronet_Densenet121API.py
+++ racing_models/dronet_Densenet121API.py
@@ -16,14 +16,12 @@ class Dronet(Model):
 
     def call(self, img):
         # Input
-        # x = DenseNet121(include_top=self.include_top, weights=None, classes = 10) (img)
-        # model_d = DenseNet121(weights='imagenet', include_top=False, input_shape=(128, 128, 3))
-        # img = tf.keras.applications.densenet.preprocess_input(img)
         # print("==============img.shape:", img.shape)
+        '''
         print('{} | img.shape: {},'.format(
             datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             img.shape))
-
+        '''
         model_d = self.denseNet121(img)
         '''
         model_d = tf.keras.layers.Activation('relu')(model_d)
@@ -35,25 +33,10 @@ class Dronet(Model):
 
         return model_d
 
-        '''
-        model_d = tf.keras.layers.Activation('relu')(model_d)
-        model_d = self.dense0(model_d)
-        model_d = self.dense1(model_d)
-        gate_pose = self.dense2(model_d)
-        return gate_pose
-        '''
-
     def create_model(self, num_outputs):
         print('[Dronet] Starting dronet')
 
-        # self.dense0 = tf.keras.layers.Dense(units=64, activation='relu')
-        # self.dense1 = tf.keras.layers.Dense(units=32, activation='relu')
-        # self.dense2 = tf.keras.layers.Dense(units=num_outputs, activation='linear')
+        #  input_shape=(224, 224, 3),
+        self.denseNet121 = DenseNet121(include_top=self.include_top, weights=None, classes=num_outputs)
 
-        self.denseNet121 = DenseNet121(include_top=self.include_top, input_shape=(224, 224, 3), weights=None, classes=num_outputs)
-        '''
-        self.dense0 = DenseNet121(include_top=self.include_top, weights=None, classes=num_outputs)
-        self.dense1 = DenseNet121(include_top=self.include_top, weights=None, classes=num_outputs)
-        self.dense2 = DenseNet121(include_top=self.include_top, weights=None, classes=num_outputs)
-        '''
         print('[Dronet] Done with dronet')
