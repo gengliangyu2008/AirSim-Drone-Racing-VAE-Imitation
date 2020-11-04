96af46b8ad0325e343a58bb630d423cf69e09e27
Gary <gengliangyu2008@gmail.com>
2020-11-05 00:20:07 +0800

    remove test time py file

--- cmvae/TestTime.py
+++
@@ -1,8 +0,0 @@
-import time
-#import datetime
-from datetime import datetime
-from time import gmtime,strftime
-
-if __name__ == '__main__':
-    # print(strftime("%Y-%m-%d %H:%M:%S", time()))
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
